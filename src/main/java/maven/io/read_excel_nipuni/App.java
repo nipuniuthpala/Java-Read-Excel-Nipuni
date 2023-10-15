4f8a2863c66d7931db98f25df1c868e7e4324c27
nipuniuthpala <48875132+nipuniuthpala@users.noreply.github.com>
2023-10-15 16:09:03 +0100

    first commit

---
+++ src/main/java/maven/io/read_excel_nipuni/App.java
@@ -0,0 +1,99 @@
+package maven.io.read_excel_nipuni;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static final String EXCEL_FILE_PATH = "./readable_writable_excel.xlsx";
+	
+    public static void main( String[] args ) throws EncryptedDocumentException, InvalidFormatException, IOException
+    {
+        // System.out.println( "Hello World!" );
+    	// Creating a Workbook from an Excel file (.xls or .xlsx)
+        Workbook workbook = WorkbookFactory.create(new File(EXCEL_FILE_PATH));
+
+        // Retrieving the number of sheets in the Workbook
+        System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
+
+
+        Iterator<Sheet> sheetIterator = workbook.sheetIterator();
+        System.out.println("Retrieving Sheets using Iterator");
+        while (sheetIterator.hasNext()) {
+            Sheet sheet = sheetIterator.next();
+            System.out.println("=> " + sheet.getSheetName());
+        }
+        
+
+
+        
+
+        Sheet sheet = workbook.getSheetAt(0);
+
+        // Create a DataFormatter to format and get each cell's value as String
+        DataFormatter dataFormatter = new DataFormatter();
+
+        // 1. You can obtain a rowIterator and columnIterator and iterate over them
+        System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
+        Iterator<Row> rowIterator = sheet.rowIterator();
+        while (rowIterator.hasNext()) {
+            Row row = rowIterator.next();
+
+            // Now let's iterate over the columns of the current row
+            Iterator<Cell> cellIterator = row.cellIterator();
+
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                String cellValue = dataFormatter.formatCellValue(cell);
+                System.out.print(cellValue + "\t");
+            }
+            System.out.println();
+        }
+        
+
+        workbook.close();
+    }
+    
+    private static void printCellValue(Cell cell) {
+        switch (cell.getCellTypeEnum()) {
+            case BOOLEAN:
+                System.out.print(cell.getBooleanCellValue());
+                break;
+            case STRING:
+                System.out.print(cell.getRichStringCellValue().getString());
+                break;
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    System.out.print(cell.getDateCellValue());
+                } else {
+                    System.out.print(cell.getNumericCellValue());
+                }
+                break;
+            case FORMULA:
+                System.out.print(cell.getCellFormula());
+                break;
+            case BLANK:
+                System.out.print("");
+                break;
+            default:
+                System.out.print("");
+        }
+
+        System.out.print("\t");
+    }
+}
